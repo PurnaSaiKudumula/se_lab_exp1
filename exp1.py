ed42d465929a3c6fc012fb303fd7d21578da5f8f
PurnaSaiKudumula <purnasaikudumula@gmail.com>
2025-07-20 08:33:42 +0530

    Refactor temperature modeling to read parameters from input file

--- exp1.py
+++ exp1.py
@@ -1,13 +1,14 @@
-# Version 2 : Keyboard Input:
-
 def Temperature_modeling(a, b, c, time):
-	# Example model: quadratic equation for demonstration
-	return a * time ** 2 + b * time + c
+    # Example model: quadratic equation
+    return a * time ** 2 + b * time + c
 
-time = int(input('Enter Time:'))
-a = float(input('Enter a:'))
-b = float(input('Enter b:'))
-c = float(input('Enter c:'))
-temp = Temperature_modeling(a, b, c, time)
-print(f"the Temperature at time {time} is {temp}")
+with open('input.txt', 'r') as file:
+    line = file.readline().strip()
+    a, b, c, time = line.split()
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    time = int(time)
 
+    temp = Temperature_modeling(a, b, c, time)
+    print(f'The Temperature at time {time} is {temp}')
