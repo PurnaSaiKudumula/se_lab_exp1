6859fd7841362473a9284937be9eee6e06314d91
PurnaSaiKudumula <purnasaikudumula@gmail.com>
2025-07-20 08:28:48 +0530

    Refactor temperature modeling to accept user input for parameters

--- exp1.py
+++ exp1.py
@@ -1,9 +1,13 @@
-# Version 1: Hardcoding the variables:
+# Version 2 : Keyboard Input:
 
-a=1
-b=2
-c=3
-time = 4
-temp = Temperature_modeling(a,b,c,time)
+def Temperature_modeling(a, b, c, time):
+	# Example model: quadratic equation for demonstration
+	return a * time ** 2 + b * time + c
+
+time = int(input('Enter Time:'))
+a = float(input('Enter a:'))
+b = float(input('Enter b:'))
+c = float(input('Enter c:'))
+temp = Temperature_modeling(a, b, c, time)
 print(f"the Temperature at time {time} is {temp}")
 
