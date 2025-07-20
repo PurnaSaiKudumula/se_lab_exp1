b877ea78600a774b5e74eb1f63f5a1451d09cbdc
PurnaSaiKudumula <purnasaikudumula@gmail.com>
2025-07-20 08:26:50 +0530

    Refactor temperature modeling function and remove file input handling

--- exp1.py
+++ exp1.py
@@ -1,23 +1,7 @@
+#Evaluation Function :
 
-def temperature_model(t, a, h, k):
-    temp = -a * (t - h)**2 + k
-    return round(temp, 2)
-
-with open("input.txt", "r") as file:
-    lines = [line.strip() for line in file.readlines()]
-
-sets = [lines[i:i+5] for i in range(0, len(lines), 5)]
-
-for idx, s in enumerate(sets):
-    a = float(s[0])
-    h = int(s[1])
-    k = float(s[2])
-    start_hour = int(s[3])
-    end_hour = int(s[4])
-
-    print(f"\n--- Set {idx + 1} ---")
-    print("Hour\tTemperature (°C)")
-    for hour in range(start_hour, end_hour + 1):
-        print(f"{hour:02d}\t{temperature_model(hour, a, h, k)}")
+def Temperature_modeling(a,b,c,time):
+  temp  = a*time**2+b*time+c
+  return temp
 
 
